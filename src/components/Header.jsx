73edd1fd63598039ef960c4bd8955b0e2164bd5e
jman1359 <josiahsmith1359@gmail.com>
2024-01-05 16:11:05 -0500

    Adding and editing components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+import Navigation from './Navigation';
+
+function Portfolio() {
+
+    return (
+          <h1>Josiah Smith's Portfolio</h1>
+      );
+};
+
+export default Portfolio;
