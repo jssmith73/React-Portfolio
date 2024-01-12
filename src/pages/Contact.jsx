d618c83c03585638db03842940268012367bb5a2
jman1359 <josiahsmith1359@gmail.com>
2024-01-12 15:17:23 -0500

    updated acceptance criteria changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,26 +1,60 @@
+import react, { useState } from 'react';
 import { validateEmail } from "../utils/helpers"
 
 const handleChange = (e) => {
-    if (e.target.name === 'email') {
-        const isValid = validateEmail(e.target.value);
-        if (!isValid) {
-            setErrorMessage('Your email is invalid.');
-    } else {
-        setErrorMessage('');
-    }
-       if(!e.target.value.length) { 
-            setErrorMessage(`${e.target.name} is required.`);
-       } else {
-            setErrorMessage('');
-       }
-        if (!errorMessage) {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-            console.log('Handle Form', formState);
-        }
-    }
+    // if (e.target.name === 'email') {
+    //     const isValid = validateEmail(e.target.value);
+    //     if (!isValid) {
+    //         setErrorMessage('Your email is invalid.');
+    //     } else {
+    //         setErrorMessage('');
+    //     }
+    //     if (!e.target.value.length) {
+    //         setErrorMessage(`${e.target.name} is required.`);
+    //     } else {
+    //         setErrorMessage('');
+    //     }
+    //     if (!errorMessage) {
+    //         setFormState({ ...formState, [e.target.name]: e.target.value });
+    //         console.log('Handle Form', formState);
+    //     }
+    // }
+    setInputValue(e.target.value);
+
 };
 
-export default function Contact() {
+// const input = document.querySelector('form-control');
+
+// const handleName = () => {
+//     if (input.value='')
+//       alert('Please enter your name!');
+// };
+// const handleEmail = () => {
+//     if (input.value='')
+//     alert('Please enter your email!');
+// };
+// const handleMessage = () => {
+//     if (input.value='')
+//     alert('Please enter a message!');
+// };
+
+const FormWithErrorMessage = () => {
+    const [inputValue, setInputValue] = useState('');
+    const [showError, setShowError] = useState(false);
+
+    const handleMouseLeave = () => {
+        if (inputValue.trim() === '') {
+            setShowError(true);
+        } else {
+            setShowError(false);
+        }
+    };
+
+    const handleChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    // function Contact() {
     return (
         <div className="container-fluid page-header py-5 mb-5">
             <div className="container py-5">
@@ -35,15 +69,22 @@ export default function Contact() {
                                 <div className="section-title text-start">
                                     <h1 className="display-5 mb-4">Contact Me</h1>
                                 </div>
-                                <p className="mb-4">Please contact me via the form below and I will respond as soon as possible! <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
-                                <form id="ajax-contact" method="post" action="mailer.php">
+                                <p className="mb-4">Please contact me via the form below and I will respond as soon as possible!</p>
+                                <form id="ajax-contact" method="post" action="mailer.php" >
                                     <div className="row g-3">
 
                                         {/* Name Enter */}
 
                                         <div className="col-md-6">
                                             <div className="form-floating">
-                                                <input type="text" className="form-control" id="name" placeholder="Your Name" />
+                                                <input type="text"
+                                                    className="form-control"
+                                                    id="name"
+                                                    placeholder="Your Name"
+                                                    value={inputValue}
+                                                    onChange={handleChange}
+                                                    onMouseLeave={handleMouseLeave} />
+                                                {showError && <div style={{ color: 'red' }}>Please enter a value!</div>}
                                                 <label htmlFor="name">Your Name</label>
                                             </div>
                                         </div>
@@ -52,14 +93,17 @@ export default function Contact() {
 
                                         <div className="col-md-6">
                                             <div className="form-floating">
-                                                <input type="email" className="form-control" id="email" placeholder="Your Email" onBlur={handleChange}/>
+                                                <input
+                                                    type="email"
+                                                    className="form-control"
+                                                    id="email"
+                                                    placeholder="Your Email"
+                                                    value={inputValue}
+                                                    onChange={handleChange}
+                                                    onMouseLeave={handleMouseLeave}
+                                                />
+                                                {showError && <div style={{ color: 'red' }}>Please enter a value!</div>}
                                                 <label htmlFor="email">Your Email</label>
-                                            </div>
-                                        </div>
-                                        <div className="col-12">
-                                            <div className="form-floating">
-                                                <input type="text" className="form-control" id="subject" placeholder="Subject" />
-                                                <label htmlFor="subject">Subject</label>
                                             </div>
                                         </div>
 
@@ -67,7 +111,16 @@ export default function Contact() {
 
                                         <div className="col-12">
                                             <div className="form-floating">
-                                                <textarea className="form-control" placeholder="Leave a message here" id="message" style={{ height: "100px" }}></textarea>
+                                                <textarea 
+                                                className="form-control" 
+                                                placeholder="Leave a message here" 
+                                                id="message" 
+                                                style={{ height: "100px" }}
+                                                ovalue={inputValue}
+                                                onChange={handleChange}
+                                                onMouseLeave={handleMouseLeave}>                                    
+                                                </textarea>
+                                                {showError && <div style={{ color: 'red' }}>Please enter a value!</div>}
                                                 <label htmlFor="message">Message</label>
                                             </div>
                                         </div>
@@ -85,3 +138,6 @@ export default function Contact() {
         </div>
     )
 }
+
+
+export default FormWithErrorMessage;
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -18,6 +18,33 @@ const DownloadableFile = () => {
 
   return (
     <div>
+      <section className="technical-skills">
+      <div class="container-fluid page-header py-5 mb-5">
+        <div class="container py-5">
+          <h1 class="display-3 text-white mb-3 animated slideInDown text-center">Technical Skills</h1>ject
+        </div>
+      </div>
+        <ul className='prof'>
+          <li className='prof-li'>Full Stack Development</li>
+          <li className='prof-li'>Salesforce</li>
+          <li className='prof-li'>APIs</li>
+          <li className='prof-li'>CRM</li>
+          <li className='prof-li'>Digital Transformation</li>
+          <li className='prof-li'>MS Office Suite</li>
+          <li className='prof-li'>HubSpot</li>
+          <li className='prof-li'>SQL</li>
+          <li className='prof-li'>HTML</li>
+          <li className='prof-li'>Crystal Reports</li>
+          <li className='prof-li'>JavaScript</li>
+          <li className='prof-li'>Website Design</li>
+          <li className='prof-li'>Business Analysis</li>
+          <li className='prof-li'>E-Commerce</li>
+          <li className='prof-li'>Financial Reporting</li>
+          <li className='prof-li'>Process Analysis</li>
+          <li className='prof-li'>Application Integration</li>
+        </ul>
+      </section>
+
       <div class="container-fluid page-header py-5 mb-5">
         <div class="container py-5">
           <h1 class="display-3 text-white mb-3 animated slideInDown text-center">Resume</h1>ject
